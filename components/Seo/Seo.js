2fa92e8b33feaf4fb9db49ac8c255bb3c5c29014
Pav Swies <pav.swies@gmail.com>
2021-09-11 13:25:14 +0100

    add seo component and populate content

--- components/Seo/Seo.js
+++ components/Seo/Seo.js
@@ -1,10 +1,33 @@
-import Head from 'next-head';
+import Head from 'next/head';
 
-const Seo = ({}) => {
+const Seo = ({ title, description, keywords, featuredImage }) => {
+  const url = 'https://swies.io';
+  const formattedTitle = title ? `${title} — Pawel Swies` : 'Pawel Swies — Web Developer';
   return (
     <>
       <Head>
-        <title>{title ? `${title} — Pawel Swies` : 'Pawel Swies — Web Developer'}</title>
+        <title>{formattedTitle}</title>
+        <meta charSet="utf-8" />
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        {description && <meta content={description} name="description" />}
+        {keywords && <meta content={keywords} name="keywords" />}
+        <meta content="follow, index" name="robots" />
+        <link href="/favicon.ico" rel="shortcut icon" />
+        <link href={url} rel="canonical" />
+        <meta content="en_US" property="og:locale" />
+        <meta content={formattedTitle} property="og:title" />
+        <meta content={description} property="og:description" />
+        <meta content={url} property="og:url" />
+        {featuredImage && (
+          <>
+            <meta content={featuredImage} property="og:image" />
+            <meta content={description} property="og:image:alt" />
+          </>
+        )}
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="@p_swies" name="twitter:site" />
+        <meta content="@p_swies" name="twitter:creator" />
       </Head>
     </>
   );
