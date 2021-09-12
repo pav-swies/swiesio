9eda49bb089f3a793084506f6848380fd1cf2e03
Pav Swies <pav.swies@gmail.com>
2021-09-12 10:11:28 +0100

    roll back to display swap on google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=optional" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
