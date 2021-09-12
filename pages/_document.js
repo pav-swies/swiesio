8c8e3ba272949dd36e80c6bba91c1df6de02d596
Pav Swies <pav.swies@gmail.com>
2021-09-12 10:36:04 +0100

    swap over to self hosted font over google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap" rel="stylesheet" />
+          <link rel="preload" href="/fonts/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
