ed18c0628afa799c4cf88fa2b8a977b88ec81cb7
Pav Swies <pav.swies@gmail.com>
2021-09-13 17:00:53 +0100

    move favicon files into their own folder

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,14 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+          <link rel="manifest" href="favicon/site.webmanifest" />
+          <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#000000" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#ffffff" />
-          <link rel="preload" href="/fonts/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="fonts/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
