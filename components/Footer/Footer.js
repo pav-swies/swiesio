e27164b298ce8fcabfcac052518e1fc2fd88e93f
Pav Swies <pav.swies@gmail.com>
2021-09-14 16:18:16 +0100

    convert footer links to a mapped object to avoid repeating code

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,37 +4,41 @@ import ExternalLink from '../ExternalLink';
 import style from './footer.module.css';
 
 const Footer = () => {
+  const links = [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/pav-swies',
+    },
+    {
+      text: 'CodeSandbox',
+      url: 'https://codesandbox.io/u/pav-swies',
+    },
+    {
+      text: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/pawel-swies-63608016b/',
+    },
+    {
+      text: 'Twitter',
+      url: 'https://twitter.com/p_swies',
+    },
+    {
+      text: 'Mail',
+      url: 'mailto:pav.swies@gmail.com',
+    },
+  ];
   return (
     <footer>
       <ContentContainer>
         <TwoColumnLayout>
           <div className={style.footerInner}>
             <ul className={style.footerList}>
-              <li>
-                <ExternalLink link="https://github.com/pav-swies" highlighted>
-                  GitHub
-                </ExternalLink>
-              </li>
-              <li>
-                <ExternalLink link="https://codesandbox.io/u/pav-swies" highlighted>
-                  CodeSandbox
-                </ExternalLink>
-              </li>
-              <li>
-                <ExternalLink link="https://www.linkedin.com/in/pawel-swies-63608016b/" highlighted>
-                  LinkedIn
-                </ExternalLink>
-              </li>
-              <li>
-                <ExternalLink link="https://twitter.com/p_swies" highlighted>
-                  Twitter
-                </ExternalLink>
-              </li>
-              <li>
-                <ExternalLink link="mailto:pav.swies@gmail.com" highlighted>
-                  Mail
-                </ExternalLink>
-              </li>
+              {links.map((link, idx) => (
+                <li key={idx}>
+                  <ExternalLink link={link.url} highlighted>
+                    {link.text}
+                  </ExternalLink>
+                </li>
+              ))}
             </ul>
             <div className={style.footerDate}>©{new Date().getFullYear()}</div>
           </div>
