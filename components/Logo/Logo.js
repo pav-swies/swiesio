af6a03484ea412fc9b84e580579fdc4e84c28f8e
Pav Swies <pav.swies@gmail.com>
2021-09-14 18:51:37 +0100

    change colour variable name, adjust icon size and add easteregg

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,8 +1,20 @@
 import style from './logo.module.css';
 
 const Logo = () => {
+  const randomColor = () => {
+    const colors = ['gold', 'tomato', 'skyblue', 'darkorange', 'lightgreen', 'greenyellow'];
+    const root = document.documentElement;
+    const currentColor = getComputedStyle(root).getPropertyValue('--color-accent');
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+
+    if (newColor === currentColor) {
+      return randomColor();
+    }
+
+    root.style.setProperty('--color-accent', newColor);
+  };
   return (
-    <div className={style.logo} aria-hidden="true">
+    <div className={style.logo} aria-hidden="true" onClick={() => randomColor()}>
       PS
     </div>
   );
